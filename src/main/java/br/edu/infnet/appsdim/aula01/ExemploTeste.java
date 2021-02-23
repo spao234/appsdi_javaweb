d55c1955ff0e0947e4809f00591cd6604dcec072
spao234 <wandersontrindadevitorino@gmail.com>
2021-02-23 09:13:05 -0300

    Alteracao da Classe Testes

--- src/main/java/br/edu/infnet/appsdim/aula01/ExemploTeste.java
+++ src/main/java/br/edu/infnet/appsdim/aula01/ExemploTeste.java
@@ -1,14 +1,33 @@
 package br.edu.infnet.appsdim.aula01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExemploTeste {
 	
 	public static void main(String[] args) {
 		
-		Aluno aluno = new Aluno();
-		aluno.setId(1);
-		aluno.setNome("Wanderson Vitorino");
-		aluno.setEmail("wanderson.vitorino@al.infnet.edu.br");
-		System.out.println(aluno);
+		// Entrada dos dados
+		String[] nomes = {"Wang", "Maria", "José"};
+	
+		String[] emails = {"wang@gmail.com", "maria@gmail.com", "jose@gmail.com"};
+		
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		//Criando Objetos : populando a coleção
+		for(int i = 0; i < nomes.length; i++){
+			Aluno aluno = new Aluno();
+			aluno.setId(i+1);
+			aluno.setNome(nomes[i]);
+			aluno.setEmail(emails[i]);
+			alunos.add(aluno);
+		
+		}
+		
+		//Leitura da coleção
+		for(Aluno item : alunos) {
+			System.out.println(item);
+		}
 	}
 
 }
